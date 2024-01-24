6241caa6209ecd9caaea54c2fd8d9b0a2a7b53c5
Joe Ellul-Turner <joeetwork@gmail.com>
2024-01-24 18:30:05 +0000

    removed components

---
+++ web/components/explore/explore-ui.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import React from 'react'
+
+export default function ExploreUi() {
+
+    // get created user accounts
+
+
+    // add modal for creating escrow
+
+
+    //add helius api to display the collection and metadata related 
+
+  return (
+    <div>explore-ui</div>
+  )
+}
