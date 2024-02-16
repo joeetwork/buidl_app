5febe6d3a0a74b568fa5edf64e283262e4c446f2
Joe Ellul-Turner <joeetwork@gmail.com>
2024-02-16 15:04:02 +0000

    removed sign up page

--- web/constants.ts
+++ web/constants.ts
@@ -1,8 +1,6 @@
 export const COLLECTIONS = ['2buwWpUqd9UaeyxQKiksa14sTyLxJaY27tYVkpR9ja5y'];
 
 export const PAGES = [
-  { label: 'Sign Up', path: '/sign-up' },
-
   { label: 'Explore', path: '/explore' },
 
   { label: 'Dashboard', path: '/dashboard' },
