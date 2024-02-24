213fe0553da42b64d2f52e1d446ce46ea9a4c36e
Joe Ellul-Turner <joe@joes-air.home>
2024-02-24 12:28:10 +0000

    close modal outside and with esc

--- web/components/shared/app-modal.tsx
+++ web/components/shared/app-modal.tsx
@@ -50,7 +50,7 @@ export function AppModal({
   }, [show, dialogRef, hide]);
 
   return (
-    <dialog className="modal cursor-default" ref={dialogRef}>
+    <dialog className="modal" ref={dialogRef}>
       <div className="modal-box space-y-5">
         <div className="flex justify-between w-full">
           <h3 className="font-bold text-lg">{title}</h3>
