2e018c4f015df3fc2ed37346e9ba4f122c7c9a34
Joe Ellul-Turner <joe@joes-air.home>
2024-02-26 15:54:23 +0000

    dashboard update

--- web/components/dashboard/dashboard.tsx
+++ web/components/dashboard/dashboard.tsx
@@ -1,27 +1,36 @@
 'use client';
 
 import React, { useState } from 'react';
-import { ROLES } from '@/constants';
+import { FREELANCEROLES, ROLES } from '@/constants';
 import Dev from './dev';
 import Validator from './validator';
 import Collection from './collection';
 import Tabs from '../shared/tabs';
 import Client from './client';
+import { useAccounts } from '@/hooks/get-accounts';
 
 export default function Dashboard() {
   const [activeTab, setActiveTab] = useState(0);
+  const { userAccount } = useAccounts();
 
-  const Roles = [
+  const FreelancerRoles = [
     <Dev key="dev" />,
+    <Collection key="collection" />,
+    <Validator key="validator" />,
+  ];
+
+  const ClientRoles = [
     <Client key="client" />,
     <Collection key="collection" />,
     <Validator key="validator" />,
   ];
 
+  const Roles = userAccount.data?.freelancer ? FreelancerRoles : ClientRoles;
+
   return (
     <div className="flex flex-col gap-4 pt-4 pb-10 mx-10">
       <Tabs
-        tabs={ROLES}
+        tabs={userAccount.data?.freelancer ? FREELANCEROLES : ROLES}
         setTab={setActiveTab}
         activeTab={activeTab}
         className="m-auto"
