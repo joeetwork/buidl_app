ac44507ad1013735aeeb1e4d02f6cb939a7da953
Joe Ellul-Turner <joe@joes-air.home>
2024-02-28 20:10:37 +0000

    enforce client to input contact details

--- anchor/programs/buidl/src/contexts/initialize_escrow.rs
+++ anchor/programs/buidl/src/contexts/initialize_escrow.rs
@@ -24,6 +24,10 @@ pub struct Initialize<'info> {
         init_if_needed,
         payer = initializer,
         space = Escrow::INIT_SPACE,
+        constraint = user_state.links.twitter.is_some() ||
+        user_state.links.discord.is_some() ||
+        user_state.links.telegram.is_some() ||
+        user_state.links.github.is_some(),
         seeds = [ESCROW.as_ref(), &seed.to_le_bytes()],
         bump
     )]
