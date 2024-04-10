9b12c38efc824280c98019351b0ef6f99b12cebe
Joe Ellul-Turner <joe@joes-air.home>
2024-04-10 08:13:13 +0100

    updated contract ui

--- web/components/shared/escrow-display.tsx
+++ web/components/shared/escrow-display.tsx
@@ -49,32 +49,30 @@ export default function EscrowsDisplay({
           </div>
         </div>
       </div>
-      {escrow ? (
-        <div
-          className={`bg-gray-500 rounded-lg p-4 hover:ring hover:ring-gray-700 w-full h-[210px]`}
-        >
-          <div className="w-full flex flex-col">
-            <span className="label-text cursor-default">
-              Contract Information
-            </span>
-          </div>
-          <div className="flex justify-between">
-            <div className="rounded bg-black p-4 w-[49%]">
-              {escrow.voteDeadline}
-            </div>
-            <button
-              onClick={() => window.open(escrow?.uploadWork)}
-              className={'btn btn-primary w-[49%]'}
-              disabled={!escrow.uploadWork}
-            >
-              Check Uploaded work
-            </button>
-          </div>
-          <div className="rounded bg-black p-4 mt-2 h-[100px] overflow-y-scroll">
-            {escrow.about}
-          </div>
+      <div
+        className={`bg-gray-500 rounded-lg p-4 hover:ring hover:ring-gray-700 w-full h-[210px]`}
+      >
+        <div className="w-full flex flex-col">
+          <span className="label-text cursor-default">
+            Contract Information
+          </span>
         </div>
-      ) : null}
+        <div className="flex justify-between">
+          <div className="rounded bg-black p-4 w-[49%]">
+            {escrow?.voteDeadline}
+          </div>
+          <button
+            onClick={() => window.open(escrow?.uploadWork)}
+            className={'btn btn-primary w-[49%]'}
+            disabled={escrow ? !escrow.uploadWork : true}
+          >
+            Check Uploaded work
+          </button>
+        </div>
+        <div className="rounded bg-black p-4 mt-2 h-[100px] overflow-y-scroll">
+          {escrow?.about}
+        </div>
+      </div>
     </>
   );
 }
